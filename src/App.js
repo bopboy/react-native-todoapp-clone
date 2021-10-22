074cede41d505f8c4219a2725c370ef40d7035cf
bopboy2 <scp0000@gmail.com>
2021-10-22 15:23:44 +0900

    프로젝트 배경 화면 설정

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components'
+import { theme } from './theme'
+
+const Container = styled.View`
+  flex:1;
+  background-color: ${({ theme }) => theme.background};
+  align-items: center;
+  justify-content: center;
+`
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Text>TODO</Text>
+        <StatusBar style="auto" />
+      </Container>
+    </ThemeProvider>
+  );
+}
